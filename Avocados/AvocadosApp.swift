62608242b1bd21018c352aebb0185a0c179dd1b4
Jon Sweeney <jonsweeney26@gmil.com>
2020-11-10 07:39:53 -0800

    Initial Commit

---
+++ Avocados/AvocadosApp.swift
@@ -0,0 +1,17 @@
+//
+//  AvocadosApp.swift
+//  Avocados
+//
+//  Created by Jonathan Sweeney on 11/10/20.
+//
+
+import SwiftUI
+
+@main
+struct AvocadosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
