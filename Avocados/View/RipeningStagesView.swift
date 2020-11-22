eae530bbf8472a0ecb00665d05608945eed2c476
Jon Sweeney <jonsweeney26@gmil.com>
2020-11-22 13:21:08 -0800

    Update Avocados view

---
+++ Avocados/View/RipeningStagesView.swift
@@ -0,0 +1,20 @@
+//
+//  RipeningStagesView.swift
+//  Avocados
+//
+//  Created by Jonathan Sweeney on 11/10/20.
+//
+
+import SwiftUI
+
+struct RipeningStagesView: View {
+    var body: some View {
+        Text("Ripening Stages View")
+    }
+}
+
+struct RipeningStagesView_Previews: PreviewProvider {
+    static var previews: some View {
+        RipeningStagesView()
+    }
+}
